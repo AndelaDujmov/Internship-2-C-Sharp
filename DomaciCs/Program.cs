d48c632721e6ada85e5212b0ff9c98bd9c217dc2
AndelaDujmov <andeladujmov3@gmail.com>
2022-11-17 00:08:36 +0100

    started training and added changes

--- DomaciCs/Program.cs
+++ DomaciCs/Program.cs
@@ -8,11 +8,52 @@
 };
 
 var igrači = new Dictionary<string, (string Position, int Rating)>()
-    {};
+{
+    {"Luka Modrić", ("MF", 89)},
+    {"Marcelo Brozović", ("DF", 77)},
+    {"Mateo Kovačić", ("MF", 78)},
+    {"Ivan Perišić", ("MF", 76)},
+    {"Andrej Kramarić", ("MF", 67)},
+    {"Joško Gvarilov", ("MF", 77)},
+    {"Ivan Rakitić", ("FW", 67)},
+    {"Mario Pašalić", ("MF", 87)},
+    {"Lovro Majer", ("DF", 54)},
+    {"Dominik Livaković", ("GK", 32)},
+    {"Ante Rebić", ("FW", 63)},
+    {"Josip Brekalo", ("MF", 67)},
+    {"Borna Sosa", ("DF", 78)},
+    {"Nikola Vlašić", ("FW", 87)}
+    
+};
 
 
-Dictionary<string, (string Position, int Rating)> EnterThePlayer(Dictionary<string, (string Position, int Rating)> dictionary
-)
+int returnValue(int rating)
+{
+    Random random = new Random();
+    var rankingRandom =  -0.05 +  (0.05 - (-0.05)) * random.NextDouble();
+    rating = (int)(rating + (rating * rankingRandom));
+    return rating;
+}
+
+Dictionary<string, (string Position, int Rating)> Training(Dictionary<string, (string Position, int Rating)> dictionary)
+{
+    foreach (var player in dictionary.Keys)
+    {
+        Console.WriteLine("Trening u toku...");
+        //player.Rating = returnValue(player.Rating);
+    }
+
+    Console.WriteLine("Trening uspješno odrađen!");
+    foreach (var player in dictionary)
+    {
+        Console.WriteLine($"{player.Key} ima trenutan rating od {player.Value.Rating}");
+    }
+
+    return dictionary;
+}
+
+
+Dictionary<string, (string Position, int Rating)> EnterThePlayer(Dictionary<string, (string Position, int Rating)> dictionary)
 {
     var ime = "";
     var prezime = "";
@@ -22,26 +63,31 @@ Dictionary<string, (string Position, int Rating)> EnterThePlayer(Dictionary<stri
     ime = Console.ReadLine();
     Console.WriteLine("Unesite ime igrača!");
     prezime = Console.ReadLine();
-    var fullName = ime.Trim() + "" + prezime.Trim();
+    var fullName = ime.Trim() + ' ' + prezime.Trim();
     Console.WriteLine("Unesite poziciju igrača: GK, DF, MF ili FW!");
     position = Console.ReadLine();
 
-  
+
     Console.WriteLine("Unesite rating igrača od 1 do 100!");
     int.TryParse(Console.ReadLine(), out rating);
-    
+
     if (rating < 1 || rating > 100)
         return dictionary;
 
-    if (dictionary.Count is 0)
-        dictionary.Add(fullName, (position, rating));
+    if (dictionary.Count is 26)
+    {
+        Console.WriteLine("Previše igrača uneseno! Oslobodite prostor.");
+        return dictionary;
+    }
     else
     {
-        foreach (var item in dictionary)
+        if (dictionary.ContainsKey(fullName))
         {
-            if(!item.Key.Contains(fullName))
-                dictionary.Add(fullName, (position, rating));
+            Console.WriteLine("Ovaj igrač već postoji!");
+            return dictionary;
         }
+
+        dictionary.Add(fullName, (position, rating));
     }
 
     return dictionary;
@@ -60,6 +106,7 @@ switch (izbor)
 {
     case 1:
         Console.WriteLine($"Odabrali ste izbor {izbornik[1]}");
+        igrači = Training(igrači);
         break;
     case 0:
         Console.WriteLine($"Odabrali ste izbor {izbornik[0]}");
