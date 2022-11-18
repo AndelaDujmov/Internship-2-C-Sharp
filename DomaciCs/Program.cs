845521db962ef6c51a2c4ae837f7d58d3121b043
AndelaDujmov <andeladujmov3@gmail.com>
2022-11-18 10:03:12 +0100

    training done,editing,deleting,etc

--- DomaciCs/Program.cs
+++ DomaciCs/Program.cs
@@ -1,4 +1,6 @@
-﻿var izbornik = new Dictionary<int, string>()
+﻿using System.Runtime.InteropServices.ComTypes;
+
+var izbornik = new Dictionary<int, string>()
 {
     {1, "Odradi trening"},
     {2, "Odigraj utakmicu"},
@@ -36,15 +38,14 @@ int ReturnValue(int rating)
     return rating;
 }
 
-Dictionary<string, (string Position, int Rating)> Training(Dictionary<string, (string Position, int Rating)> dictionary)
+void Training()
 {
     var updated = new Dictionary<string, (string Position, int Rating)>();
     
     Console.WriteLine($"Odabrali ste opciju {izbornik[1]}");
     
-    foreach (var dict in dictionary)
+    foreach (var dict in igrači)
     {
-        Console.WriteLine("Trening u toku...");
         updated.Add(dict.Key, (dict.Value.Position, ReturnValue(dict.Value.Rating)));
     }
 
@@ -53,26 +54,178 @@ Dictionary<string, (string Position, int Rating)> Training(Dictionary<string, (s
     {
         Console.WriteLine($"Igrač {dict.Key} ima ranking od {dict.Value.Rating}");
     }
+
+    igrači = updated;
     
-    return updated;
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+    return;
 }
 
 
 //-----odigraj utakmicu -------
 
-Dictionary<string, (string Position, int Rating)> PlayTheGame(Dictionary<string, (string Position, int Rating)> dictionary)
+void PlayTheGame()
+
 {
-    var afterGame = new Dictionary<string, (string Position, int Rating)>();
+    var team = new Dictionary<string, (string Position, int Rating)>();
+    var GK = (Name: "", Position: "", Rating: 0);
+    var DF = new List<Tuple<string, string, int>>();
+    var MF = new List<Tuple<string, string, int>>();
+    var FW = new List<Tuple<string, string, int>>();
+
     var strijelac = 0;
-    var GK
-    foreach (var player in dictionary)
+    var rezultat = 0;
+    foreach (var player in igrači)
     {
-        
+        if (team.Count == 11)
+            break;
+
+        if (player.Value.Rating > GK.Rating && player.Value.Position.Equals("GK"))
+        {
+            GK = (player.Key, player.Value.Position, player.Value.Rating);
+            team.Add(GK.Name, (GK.Position, GK.Rating));
+        }
+
+    }
+   
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+//------------statistika---------------------
+void PrintPlayersNoOrder()
+{
+    igrači.ToList().ForEach(player => Console.WriteLine($"Igrač {player.Key}"));
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+void PrintPlayersAsc()
+{
+    var ordered = igrači.OrderBy(x => x.Value.Rating).ToDictionary(x => x.Key, x => (x.Value.Position, x.Value.Rating));
+    
+    ordered.ToList().ForEach(player => Console.WriteLine($"Igrač {player.Key}  s ratingom {player.Value.Rating}"));
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+//ne radi kako triba
+void PrintPlayersDesc()
+{
+    var descending = igrači.OrderByDescending(u => u.Value).ToDictionary(z => z.Key, y => y.Value);
+    descending.ToList().ForEach(player => Console.WriteLine($"Igrač {player.Key} s ratingom {player.Value.Rating}"));
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+//ne razumijem u cemu se razlikuje od 1.
+void PrintPlayersByName()
+{
+    igrači.ToList().ForEach(player => Console.WriteLine($"Igrač {player.Key} na poziciji {player.Value.Position} s ratingom {player.Value.Rating}"));
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+void PrintPlayers()
+{
+    var input = 0;
+    Console.Clear();
+    Console.WriteLine("Dobrodošli na izbornik Ispis svih igrača, odaberite jednu od sljedećih opcija:");
+    Console.WriteLine("1 - Ispis igrača bez redoslijeda");
+    Console.WriteLine("2 - Ispis po ratingu uzlazno");
+    Console.WriteLine("3 - Ispis po ratingu silazno");
+    Console.WriteLine("4 - Ispis igrača po imenu i prezimenu");
+    Console.WriteLine("5 - Ispis igrača po ratingu");
+    Console.WriteLine("6 - Ispis igrača po poziciji");
+    Console.WriteLine("7 - Ispis trenutnih prvih 11 igrača");
+    Console.WriteLine("8 - Ispis strijelaca i koliko golova imaju");
+    Console.WriteLine("9 - Ispis svih rezultata ekipe");
+    Console.WriteLine("10 - Ispis rezultat svih ekipa");
+    Console.WriteLine("11 - Ispis tablice grupe");
+    
+    int.TryParse(Console.ReadLine(), out input);
+
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("Izabrali ste opciju Ispis igrača bez redoslijeda");
+            PrintPlayersNoOrder();
+            break;
+        case 2:
+            Console.WriteLine("Izabrali ste opciju Ispis igrača uzlazno");
+            PrintPlayersAsc();
+            break;
+        case 3:
+            Console.WriteLine("Izabrali ste opciju Ispis igrača silazno");
+            PrintPlayersDesc();
+            break;
+        case 4:
+            Console.WriteLine("Izabrali ste opciju Ispis igrača po Imenu i prezimenu");
+            PrintPlayersByName();
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+    }
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+    return;
+}
+
+void Statistics()
+{
+    var input = 0;
+    Console.WriteLine($"Dobrodošli na izbornik {izbornik[3]}, izaberite jednu od ponuđenih opcija:");
+    Console.WriteLine("1 - Ispis svih igrača");
+    int.TryParse(Console.ReadLine(), out input);
+
+    if (input is 1)
+        PrintPlayers();
+    else
+    {
+        Console.WriteLine("Niste izabrali ni jednu od ponuđenih opcija"); 
+        Console.WriteLine("Želite li povratak na glavni meni? y/n");
+        var ans = Console.ReadLine();
+
+        if (ans.Equals("y"))
+            MainMenu();
+        return;
     }
 }
 
-//----------------unos igrača-----------------
-Dictionary<string, (string Position, int Rating)> EnterThePlayer(Dictionary<string, (string Position, int Rating)> dictionary)
+//----------------kontrola igrača-----------------
+void EnterThePlayer()
 {
     var ime = "";
     var prezime = "";
@@ -80,40 +233,254 @@ Dictionary<string, (string Position, int Rating)> EnterThePlayer(Dictionary<stri
     var rating = 0;
     Console.WriteLine("Unesite ime igrača!");
     ime = Console.ReadLine();
-    Console.WriteLine("Unesite ime igrača!");
+    Console.WriteLine("Unesite prezime igrača!");
     prezime = Console.ReadLine();
     var fullName = ime.Trim() + ' ' + prezime.Trim();
     Console.WriteLine("Unesite poziciju igrača: GK, DF, MF ili FW!");
-    position = Console.ReadLine();
+    position = Console.ReadLine().ToUpper();
 
+    if (!position.Equals("GK") || !position.Equals("DF") || !position.Equals("MF") || !position.Equals("FW"))
+    {
+        Console.WriteLine("Nije točno unesena pozicija, mora biti jedna od navedenih!");
+        
+        Console.WriteLine("Unesite poziciju igrača: GK, DF, MF ili FW!");
+        position = Console.ReadLine().ToUpper();
+    }
 
     Console.WriteLine("Unesite rating igrača od 1 do 100!");
     int.TryParse(Console.ReadLine(), out rating);
 
     if (rating < 1 || rating > 100)
-        return dictionary;
+    {
+         
+        Console.WriteLine("Želite li povratak na glavni meni? y/n");
+        var ans = Console.ReadLine();
 
-    if (dictionary.Count is 26)
+        if (ans.Equals("y"))
+            MainMenu();
+        return;
+    }
+
+    if (igrači.Count is 26)
     {
         Console.WriteLine("Previše igrača uneseno! Oslobodite prostor.");
-        return dictionary;
+         
+        Console.WriteLine("Želite li povratak na glavni meni? y/n");
+        var ans = Console.ReadLine();
+
+        if (ans.Equals("y"))
+            MainMenu();
+        return;
     }
     else
     {
-        if (dictionary.ContainsKey(fullName))
+        if (igrači.ContainsKey(fullName))
         {
             Console.WriteLine("Ovaj igrač već postoji!");
-            return dictionary;
+            Console.WriteLine("Želite li povratak na glavni meni? y/n");
+            var ans = Console.ReadLine();
+
+            if (ans.Equals("y"))
+                MainMenu();
+            return;
         }
 
-        dictionary.Add(fullName, (position, rating));
+        igrači.Add(fullName, (position, rating));
     }
-
-    return dictionary;
 }
 
+void DeleteThePlayer()
+{
+    var newDict = new Dictionary<string, (string Position, int Rating)>();
+    
+    Console.WriteLine("Unesite Ime igrača kojeg želite izbrisati!");
+    var name = Console.ReadLine();
+    Console.WriteLine("Unesite Prezime igrača kojeg želite izbrisati!");
+    var surname = Console.ReadLine();
+    var fullName = name.Trim() + ' ' + surname.Trim();
+
+    foreach (var element in igrači)
+    {
+        if(!element.Key.Equals(fullName))
+            newDict.Add(element.Key, (element.Value.Position, element.Value.Rating));
+    }
+    
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+    
+    igrači = newDict;
+}
+
+//-----uređivanje------
+
+void EditThePlayerRank()
+{
+    Console.WriteLine("Odabrali ste opciju Uredi Rank igrača");
+    var updated = new Dictionary<string, (string Position, int Rating)>();
+
+    Console.WriteLine("Unesite ime igrača kojeg  biste uredili");
+    var ime = Console.ReadLine();
+    Console.WriteLine("Unesite prezime igrača kojeg biste uredili!");
+    var prezime = Console.ReadLine();
+   
+    var fullname = ime.Trim() + ' ' + prezime.Trim();
+
+    foreach (var element in igrači)
+    {
+        if (element.Key.Equals(fullname))
+        {
+            Console.WriteLine("Unesite novi rank igrača");
+            var rank = 0;
+            int.TryParse(Console.ReadLine(), out rank);
+            if (rank >= 1 && rank <=100)
+                updated.Add(element.Key, (element.Value.Position, rank));
+            else
+            {
+                Console.WriteLine("Rank izvan predviđenih granica! Pokušati ponovo!");
+                EditThePlayer();
+            }
+            
+        }
+        else
+            updated.Add(element.Key, (element.Value.Position, element.Value.Rating));
+        
+        
+    }
+
+    igrači = updated;
+    
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+}
+
+void EditThePlayerPosition()
+{
+    Console.WriteLine("Odabrali ste opciju Uredi poziciju igrača (GK, DF, MF ili FW)");
+    var updated = new Dictionary<string, (string Position, int Rating)>();
+
+    Console.WriteLine("Unesite ime igrača kojeg  biste uredili");
+    var ime = Console.ReadLine();
+    Console.WriteLine("Unesite prezime igrača kojeg biste uredili!");
+    var prezime = Console.ReadLine();
+
+    var fullname = ime.Trim() + ' ' + prezime.Trim();
+
+    foreach (var element in igrači)
+    {
+        if (element.Key.Equals(fullname))
+        {
+            Console.WriteLine("Unesite novu poziciju igrača");
+            var pozicija = Console.ReadLine().ToUpper();
+            if (pozicija.Equals("GK") || pozicija.Equals("DF") || pozicija.Equals("MF") || pozicija.Equals("FW"))
+                updated.Add(element.Key, (pozicija, element.Value.Rating));
+            else
+            {
+                Console.WriteLine("Pogrešno unesena pozicija! Pokušati ponovo!");
+                EditThePlayer();
+            }
+            
+        }
+        else
+            updated.Add(element.Key, (element.Value.Position, element.Value.Rating));
+    }
+
+    igrači = updated;
+    
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+
+}
+
+void EditThePlayerName()
+{
+    
+    Console.WriteLine("Odabrali ste opciju Uredi ime i prezime igrača");
+    var updated = new Dictionary<string, (string Position, int Rating)>();
+    
+    Console.WriteLine("Unesite ime igrača kojeg  biste uredili");
+    var ime = Console.ReadLine();
+    Console.WriteLine("Unesite prezime igrača kojeg biste uredili!");
+    var prezime = Console.ReadLine();
+
+    var fullname = ime.Trim() + ' ' + prezime.Trim();
+
+    if (!igrači.ContainsKey(fullname))
+    {
+        Console.WriteLine("Nije moguće pronaći ime igrača! Pokušajte ponovo!");
+        EditThePlayer();
+    }
+
+    foreach (var element in igrači)
+    {
+        if (element.Key.Equals(fullname))
+        {
+            Console.WriteLine("Unesite novo ime igrača");
+            ime = Console.ReadLine();
+            Console.WriteLine("Unesite novo prezime igrača!");
+            prezime = Console.ReadLine();
+            fullname = ime.Trim() + ' ' + prezime.Trim();
+            
+            updated.Add(fullname, (element.Value.Position, element.Value.Rating));
+        }
+        else
+            updated.Add(element.Key, (element.Value.Position, element.Value.Rating));
+    }
+
+    igrači = updated;
+
+    Console.WriteLine("Želite li povratak na glavni meni? y/n");
+    var ans = Console.ReadLine();
+
+    if (ans.Equals("y"))
+        MainMenu();
+
+
+}
+
+void EditThePlayer()
+{
+    Console.Clear();
+    Console.WriteLine("Dobrodošli na izbornik Uređivanja igrača! Izaberite jedno od ponuđenih opcija:");
+    Console.WriteLine("1 - Uredi ime i prezime igrača");
+    Console.WriteLine("2 - Uredi poziciju igrača (GK, DF, MF ili FW)");
+    Console.WriteLine("3 - Uredi rating igrača (od 1 do 100)");
+
+    var izbor = 0;
+    int.TryParse(Console.ReadLine(), out izbor);
+
+    switch (izbor)
+    {
+        case 1:
+            EditThePlayerName();
+            break;
+        case 2:
+            EditThePlayerPosition();
+            break;
+        case 3:
+            EditThePlayerRank();
+            break;
+        default:
+            Console.WriteLine("Niste odabrali ni jedan od opcija!");
+            Console.WriteLine("Želite li povratak na glavni izbornik? y/n");
+            var ans = Console.ReadLine();
+            if (ans.Equals('y'))
+                MainMenu();
+            break;
+    }
+}
 
 //-----glavni izbornik-------
+
+void MainMenu(){
 Console.WriteLine("Dobrodošli na glavni izbornik! Izaberite jednu od navedenih opcija!");
 foreach (var i in izbornik)
 {
@@ -128,18 +495,20 @@ switch (izbor)
     case 1:
         Console.Clear();
         Console.WriteLine($"Odabrali ste izbor {izbornik[1]}");
-        igrači = Training(igrači);
+        Training();
         break;
     case 0:
         Console.WriteLine($"Odabrali ste izbor {izbornik[0]}");
         break;
-    case 2: 
+    case 2:
         Console.Clear();
         Console.WriteLine($"Odabrali ste izbor {izbornik[2]}");
-        igrači = PlayTheGame(igrači);
+        PlayTheGame();
         break;
-    case 3: 
+    case 3:
+        Console.Clear();
         Console.WriteLine($"Odabrali ste izbor {izbornik[3]}");
+        Statistics();
         break;
     case 4:
         Console.Clear();
@@ -153,17 +522,28 @@ switch (izbor)
         if (izbor4 is 1)
         {
             Console.WriteLine("Izabrali ste Unos novog igrača");
-            igrači = EnterThePlayer(igrači);
+            EnterThePlayer();
         }
         else if (izbor4 is 2)
+        {
             Console.WriteLine("Izabrali ste Brisanje igrača");
-        else if(izbor4 is 3)
+            DeleteThePlayer();
+        }
+        else if (izbor4 is 3)
+        {
             Console.WriteLine("Izabrali ste Uređivanje igrača");
+            EditThePlayer();
+        }
         else
             Console.WriteLine("Niste izabrali nijednu opciju! Pokušajte ponovo.");
+
         break;
     default:
         Console.WriteLine("Niste odabrali ni jedan od ponuđenih izbora! Pokušajte ponovo.");
         break;
 }
+
+}
+
+MainMenu();
 
